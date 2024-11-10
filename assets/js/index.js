90899fe1ffdee30fd0c9e4800bc999f4da20f153
Rockketz-dev <f.osorio@outlook.com>
2024-11-10 01:05:32 -0300

    add desafio

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+let nombre = prompt(
+  "Bienvenido, Ingresa tu nombre para una atención más personalizada"
+);
+
+insertarNombre = document.querySelector(".nombre-usr");
+insertarNombre.innerHTML = nombre;
+
+const precio = 400000;
+let cantidad = 0;
+const botonMas = document.querySelector("#mas");
+const botonMenos = document.querySelector("#menos");
+const mostrarCantidad = document.querySelector("#cantidad");
+const mostrarPrecioFinal = document.querySelector("#precioFinal");
+
+botonMas.addEventListener("click", () => {
+  cantidad += 1;
+  mostrarCantidad.textContent = cantidad;
+  mostrarPrecioFinal.textContent = cantidad * precio;
+});
+
+botonMenos.addEventListener("click", () => {
+  if (cantidad > 0) {
+    cantidad -= 1;
+    mostrarCantidad.textContent = cantidad;
+    mostrarPrecioFinal.textContent = cantidad * precio;
+  }
+});
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
